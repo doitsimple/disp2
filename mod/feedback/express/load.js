41cffef479df61c22c29ddbb6c6bae932d50aba4
zyp001a <yzhou01a@gmail.com>
2015-01-27 11:09:33 +0800

    modify feedback

--- mod/feedback/express/load.js
+++ mod/feedback/express/load.js
@@ -3,6 +3,32 @@ var ucfirst = libString.ucfirst;
 module.exports = function(env, config){
 	for (var name in env.feedback){
 		var feedbackConfig = env.feedback[name];
+		env.schemas[name] = {
+			"fields": [
+				{
+					"name": "_id",
+					"type": "BigInteger",
+					"default": "autoinc",
+					"auto": true
+				},
+				{
+					"name": "userid",
+					"type": "BigInt"
+				},
+				{
+					"name": "content",
+					"type": "String",
+					"text": "内容"
+				},
+				{
+					"name":"create_time",
+					"type":"DateTime",
+					"text": "创建时间",
+					"default": "now",
+					"auto": true
+				}
+			]
+		}
 		env.nodeControllerFiles[name] = true;
 		if(!config) continue;
 		if(config.frontend){
--- mod/feedback/load.js
+++ mod/feedback/load.js
@@ -3,32 +3,6 @@ var ucfirst = libString.ucfirst;
 module.exports = function(env, config){
 	for (var name in env.feedback){
 		var feedbackConfig = env.feedback[name];
-		env.schemas[name] = {
-			"fields": [
-				{
-					"name": "_id",
-					"type": "BigInteger",
-					"default": "autoinc",
-					"auto": true
-				},
-				{
-					"name": "userid",
-					"type": "BigInt"
-				},
-				{
-					"name": "content",
-					"type": "String",
-					"text": "内容"
-				},
-				{
-					"name":"create_time",
-					"type":"DateTime",
-					"text": "创建时间",
-					"default": "now",
-					"auto": true
-				}
-			]
-		}
 		if(!config) continue;
 		if(config.frontend){
 			env.apis["send"+ucfirst(name)] = {
