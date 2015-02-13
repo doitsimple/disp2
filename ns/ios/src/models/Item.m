93a7e1be75ad91ff6483a9b75d9cec5b3345be3f
zyp001a <yzhou01a@gmail.com>
2015-02-13 18:51:50 +0800

    tmp

--- ns/ios/src/models/Item.m
+++ ns/ios/src/models/Item.m
@@ -9,5 +9,12 @@
 - (void)setValue:(id)value forUndefinedKey:(NSString *)key
 {
 }
-
+- (void)setValue:(id)value forKey:(NSString *)key
+{
+	if ([key isKindOfClass:[NSString class]] == YES) {
+		[super setValue:value forKey:key];
+	}else{
+		[super setValue:[NSString stringWithFormat:@"%@" , value] forKey:key];
+	}
+}
 @end
